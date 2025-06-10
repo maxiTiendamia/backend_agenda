f9defd97a77af92022cd0ad227c8b28da7e3bd94
maximiliano836 <maxi10.02.00.g@gmail.com>
2025-06-10 17:11:09 -0300

    otro

--- admin/admin_main.py
+++ admin/admin_main.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'admin-secret')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['BASIC_AUTH_USERNAME'] = os.getenv("ADMIN_USER")
+app.config['BASIC_AUTH_PASSWORD'] = os.getenv("ADMIN_PASSWORD")
 
 init_db(app)
 init_admin(app, db)
