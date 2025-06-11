d473eec5d9d444d63595dd90d3cfd7e42b25006c
maximiliano836 <maxi10.02.00.g@gmail.com>
2025-06-11 18:22:48 -0300

    corrigiendo lenguaje database

--- api/app/database.py
+++ api/app/database.py
@@ -1,12 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-import os
 DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./test.db")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}  # ← solo si usás SQLite
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
